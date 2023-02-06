dfc42dc5f38b64c015917d812773ce03030ba044
lidor baranes <lidor@walla.net.il>
2023-02-07 01:09:23 +0200

    add pino loggger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import logger from "./logger";
+import { lookup } from "dns";
 
 const PORT = 3000;
 
 const app = express();
 
+// add pino
 app.get("/", (req, res) => {
-  console.log("hello world");
+  logger.info("hello world");
+  logger.error("hello world");
+  logger.warn("hello world");
+  logger.fatal("hello world");
+
   res.status(200).send("hello world");
 });
 
